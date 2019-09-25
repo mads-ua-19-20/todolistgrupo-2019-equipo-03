17c130610040f59060ff7f949413570f9704d362
Omar Moukket <omr7@alu.ua.es>
2019-09-25 23:33:01 +0200

    Implementado test para la barra de menu sobre una de las vistas

---
+++ src/test/java/madstodolist/NavBarWebTest.java
@@ -0,0 +1,50 @@
+package madstodolist;
+
+
+import madstodolist.authentication.ManagerUserSesion;
+import madstodolist.controller.TareaController;
+import madstodolist.model.Usuario;
+import madstodolist.service.TareaService;
+import madstodolist.service.UsuarioService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(TareaController.class)
+public class NavBarWebTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UsuarioService usuarioService;
+
+    @MockBean
+    TareaService tareaService;
+
+    @MockBean
+    private ManagerUserSesion managerUserSesion;
+
+    @Test
+    public void geTareasDevuelveNavBar() throws Exception {
+        Usuario usuario = new Usuario("domingo@ua.es");
+        usuario.setId(1L);
+
+        when(usuarioService.findById(1L)).thenReturn(usuario);
+
+        this.mockMvc.perform(get("/usuarios/1/tareas"))
+                .andDo(print())
+                .andExpect(content().string(containsString("class=\"navbar navbar-expand-lg navbar-light\" style=\"background-color:#7DCCDA\"")));
+    }
+}
