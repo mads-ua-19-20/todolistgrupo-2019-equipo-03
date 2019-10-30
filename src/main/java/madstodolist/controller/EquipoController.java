e442799e8934c99d2282d7040949447fafbb2c50
Omar Moukket <omr7@alu.ua.es>
2019-10-30 13:59:00 +0100

    Añadido código para mostrar los equipos existentes en listado de la vista

--- src/main/java/madstodolist/controller/EquipoController.java
+++ src/main/java/madstodolist/controller/EquipoController.java
@@ -2,7 +2,9 @@ package madstodolist.controller;
 
 import madstodolist.authentication.UsuarioNoLogeadoException;
 import madstodolist.controller.exception.UsuarioNotFoundException;
+import madstodolist.model.Equipo;
 import madstodolist.model.Usuario;
+import madstodolist.service.EquipoService;
 import madstodolist.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,11 +12,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class EquipoController {
     @Autowired
     UsuarioService usuarioService;
+
+    @Autowired
+    EquipoService equipoService;
 
     @GetMapping("/equipos")
     public String listadoEquipos(Model model, HttpSession session) {
@@ -22,9 +28,11 @@ public class EquipoController {
 
         if(id !=  null){
             Usuario usuario = usuarioService.findById(id);
+            List<Equipo> equipos = equipoService.findAllOrderedByName();
 
             model.addAttribute("nombreUsuario", usuario.getNombre());
             model.addAttribute("idUsuario", usuario.getId());
+            model.addAttribute("equipos", equipos);
         }
         else{
             throw new UsuarioNoLogeadoException();
