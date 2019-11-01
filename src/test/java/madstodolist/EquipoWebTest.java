7a20b32a0ba1344457ec7846650bf88361056e7b
Omar Moukket <omr7@alu.ua.es>
2019-11-01 21:48:13 +0100

    Añadidos tests para las vistas relacionadas con la gestión de pertenencia a equipos

--- src/test/java/madstodolist/EquipoWebTest.java
+++ src/test/java/madstodolist/EquipoWebTest.java
@@ -1,5 +1,6 @@
 package madstodolist;
 
+import madstodolist.authentication.ManagerUserSesion;
 import madstodolist.controller.EquipoController;
 import madstodolist.model.Equipo;
 import madstodolist.model.Usuario;
@@ -12,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,9 +21,9 @@ import java.util.List;
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(EquipoController.class)
@@ -34,6 +36,9 @@ public class EquipoWebTest {
 
     @MockBean
     private EquipoService equipoService;
+
+    @MockBean
+    private ManagerUserSesion managerUserSesion;
 
     @Test
     public void verListadoEquipos() throws Exception {
@@ -107,5 +112,84 @@ public class EquipoWebTest {
         this.mockMvc.perform(get("/equipos/1/usuarios"))
                 .andDo(print())
                 .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void botonNuevoEquipo() throws  Exception {
+        Usuario usuario = new Usuario("domingo@ua.es");
+        usuario.setId(1L);
+
+        when(usuarioService.findById(null)).thenReturn(usuario);
+
+        this.mockMvc.perform(get("/equipos"))
+                .andDo(print())
+                .andExpect(content().string(containsString("href=\"/equipos/nuevo\"")));
+    }
+
+    @Test
+    public void formCrearEquipo() throws Exception {
+        Usuario usuario = new Usuario("domingo@ua.es");
+        usuario.setId(1L);
+
+        when(usuarioService.findById(null)).thenReturn(usuario);
+
+        this.mockMvc.perform(get("/equipos/nuevo"))
+                .andDo(print())
+                .andExpect(content().string(containsString("Nombre del equipo:")))
+                .andExpect(content().string(containsString("Crear equipo")));
+    }
+
+    @Test
+    public void crearEquipoRedirect() throws  Exception {
+        Usuario usuario = new Usuario("domingo@ua.es");
+        usuario.setId(1L);
+
+        when(usuarioService.findById(null)).thenReturn(usuario);
+
+        this.mockMvc.perform(post("/equipos/nuevo")
+                .param("nombre", "Equipo prueba"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/equipos"));
+    }
+
+    @Test
+    public void eliminarseUsuarioEquipo() throws Exception {
+        Usuario usuario = new Usuario("domingo@ua.es");
+        usuario.setId(1L);
+
+        Equipo equipo = new Equipo("Proyecto Cobalto");
+        equipo.setId(1L);
+
+        List<Usuario> usuariosEquipo = new ArrayList<>();
+        usuariosEquipo.add(usuario);
+
+        when(usuarioService.findById(null)).thenReturn(usuario);
+        when(equipoService.findById(1L)).thenReturn(equipo);
+        when(equipoService.usuariosEquipo(1L)).thenReturn(usuariosEquipo);
+
+        this.mockMvc.perform(get("/equipos/1/usuarios"))
+                .andDo(print())
+                .andExpect(content().string(containsString("href=\"/equipos/1/eliminar/usuario/1")));
+
+    }
+
+    @Test
+    public void añadirseUsuarioEquipo() throws Exception {
+        Usuario usuario = new Usuario("domingo@ua.es");
+        usuario.setId(1L);
+
+        Equipo equipo = new Equipo("Proyecto Cobalto");
+        equipo.setId(1L);
+
+        List<Usuario> usuariosEquipo = new ArrayList<>();
+
+        when(usuarioService.findById(null)).thenReturn(usuario);
+        when(equipoService.findById(1L)).thenReturn(equipo);
+        when(equipoService.usuariosEquipo(1L)).thenReturn(usuariosEquipo);
+
+        this.mockMvc.perform(get("/equipos/1/usuarios"))
+                .andDo(print())
+                .andExpect(content().string(containsString("href=\"/equipos/1/agregar/usuario/1")));
     }
 }
